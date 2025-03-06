acbe99fbc9ca86e147b6c40e6093908802295530
Mario <31197372+Max131@users.noreply.github.com>
2025-03-06 13:30:35 -0600

    feat(astro.config.mjs): integrar Svelte como integración feat(package.json): añadir dependencias Svelte y configurar TypeScript feat(src/pages/index.astro): agregar imagen de portada para video build(svelte.config.js): crear configuración para Svelte refactor(tsconfig.json): mejorar configuración de TypeScript con rutas personalizadas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://webbolt.fast-page.org',
   base: '/security',
   compressHTML: false,
+
   build: {
     inlineStylesheets: 'never'
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [svelte()]
 });
