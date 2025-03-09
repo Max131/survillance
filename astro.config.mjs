1faff527087ab97f3940d96ecce3720ec99f37f9
Mario <31197372+Max131@users.noreply.github.com>
2025-03-09 13:52:15 -0600

    reactor: add main layout, config: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
@@ -10,13 +8,29 @@ export default defineConfig({
   site: 'https://webbolt.fast-page.org',
   base: '/security',
   compressHTML: false,
-
   build: {
+    assets: 'assets',
+    assetsPrefix: {
+      'css': 'css/',
+      'fallback': '/'
+    },
     inlineStylesheets: 'never'
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      // cssCodeSplit: false,
+      // cssMinify: false,
+      // minify: false,
+      // target: 'esnext'
+      // rollupOptions: {
+      //   output: {
+      //     entryFileNames: '[name].js',
+      //     assetFileNames: '[name].[ext]'
+      //   }
+      // }
+    }
   },
 
   integrations: [svelte()]
